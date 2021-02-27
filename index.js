7233f413bdfd2f3849ab04dfe7fda9fd817046de
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-02-27 13:42:16 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,181 @@
+   
+   $(document).ready(function()
+   {
+      $("#booknowLayoutGrid1").submit(function(event)
+      {
+         var isValid = $.validate.form(this);
+         return isValid;
+      });
+      $("#booknowEditbox1").validate(
+      {
+         required: true,
+         bootstrap: true,
+         type: 'custom',
+         param: /^[A-Za-zÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ \t\r\n\f]*$/,
+         length_min: '3',
+         length_max: '100',
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: 'Required'
+      });
+      $("#booknowEditbox2").validate(
+      {
+         required: true,
+         bootstrap: true,
+         type: 'number',
+         expr_min: '',
+         expr_max: '',
+         value_min: '10',
+         value_max: '',
+         length_min: '9',
+         length_max: '10',
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: '*Required'
+      });
+      $("#booknowEditbox3").validate(
+      {
+         required: true,
+         bootstrap: true,
+         type: 'custom',
+         param: /^[A-Za-zÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ \t\r\n\f0-9-, . /]*$/,
+         length_min: '3',
+         length_max: '100',
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: '*Required'
+      });
+      $("#booknowEditbox6").validate(
+      {
+         required: true,
+         bootstrap: true,
+         type: 'text',
+         length_min: '2',
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: '*Please Enter Your Event Date'
+      });
+      $("#booknowCombobox1").validate(
+      {
+         required: true,
+         bootstrap: true,
+         type: 'select',
+         disallowfirstchoice: true,
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: '*Required'
+      });
+      $("#indexEditbox1").validate(
+      {
+         required: true,
+         bootstrap: true,
+         type: 'number',
+         expr_min: '',
+         expr_max: '',
+         value_min: '',
+         value_max: '',
+         length_min: '1',
+         length_max: '2',
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: '*Required'
+      });
+      $("#indexEditbox2").validate(
+      {
+         required: true,
+         bootstrap: true,
+         type: 'custom',
+         param: /^[A-Za-zÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ \t\r\n\f0-9-, . -]*$/,
+         length_min: '3',
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: '*Required'
+      });
+      $("#indexEditbox3").validate(
+      {
+         required: false,
+         bootstrap: true,
+         type: 'number',
+         expr_min: '',
+         expr_max: '',
+         value_min: '0',
+         value_max: '',
+         length_min: '0',
+         length_max: '0',
+         color_text: '#000000',
+         color_hint: '#00FF00',
+         color_error: '#FF0000',
+         color_border: '#808080',
+         nohint: false,
+         font_family: 'Arial',
+         font_size: '13px',
+         position: 'topleft',
+         offsetx: 0,
+         offsety: 0,
+         effect: 'none',
+         error_text: ''
+      });
+   });
